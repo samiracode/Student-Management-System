be9b2c9ca0fbac35a20da9d7d44bf624e535a860
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-11-17 15:02:27 +0100

    edit functionality

--- students/views.py
+++ students/views.py
@@ -44,3 +44,21 @@ def add(request):
         'form' : StudentForm()
     })
 
+def edit(request, id):
+    if request.method == 'POST':
+      student = Student.objects.get(pk=id)
+      form = StudentForm(request.POST, instance=student)
+      if form.is_valid():
+          form.save(),
+          return render(request, 'students/edit.html', {
+              'form': form,
+              'success': True
+            })
+    else:
+        student = Student.objects.get(pk=id)
+        form = StudentForm(instance=student)
+    return render(request, 'students/edit.html', {
+        'form': form
+    })
+
+
