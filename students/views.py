a9fa6937acbeefdb9cdf4e7574a061228d186864
Samira Tavakolli <samiratavakolli.k@gmail.com>
2023-11-17 15:24:20 +0100

    delete

--- students/views.py
+++ students/views.py
@@ -61,4 +61,8 @@ def edit(request, id):
         'form': form
     })
 
-
+def delete(request, id):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=id)
+        student.delete()
+    return HttpResponseRedirect(reverse('index'))
